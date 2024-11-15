b1e03c5a11344f1abec51c8d18de06e08daa015f
Estefania <lgomez@sdmujer.gov.co>
2024-11-14 19:47:37 -0500

    Feat: se agrego el promedio

--- punto1.js
+++ punto1.js
@@ -1,8 +1,13 @@
 let numeros = [5,1,8,6,7,8,6,9];
 let sumatoria=0;
+let promedio=0;
 
 for (let i=0; i<numeros.length; i++){
     sumatoria=+numeros[i];    
 }
 
+promedio=sumatoria/numeros.length;
+
 document.getElementById("resultado").value = sumatoria;
+
+document.getElementById("promedio").value = promedio;
